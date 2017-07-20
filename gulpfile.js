3135989ea20579486475f8a9aba73f60b93494ff
Ľuboš Voška <voska.lubos@gmail.com>
2017-07-20 19:35:48 +0200

    Add sticky nav

--- gulpfile.js
+++ gulpfile.js
@@ -12,4 +12,5 @@ gulp.task('browser-sync', function() {
 gulp.task('watch', ['browser-sync'], function () {
     gulp.watch("./index.html").on('change', bs.reload);
     gulp.watch("./resources/css/*.css").on('change', bs.reload);
+     gulp.watch("./resources/js/*.js").on('change', bs.reload);
 });
---
+++ resources/js/scripts.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+    $('.js--section-seatures').waypoint( function(direction) {
+        if(direction === "down") {
+            $('nav').addClass('sticky');
+        } else {
+            $('nav').removeClass('sticky');
+        }
+    }, {
+        offset: '60px'
+    });
+});
